3991ffdd576a28332281732b233d0d87b40f1866
Michael HOARAU <michael.hoarau@gmail.com>
2023-11-24 06:34:25 +0100

    Info link not working / reloads whole page

--- src/components/contexts/OfflineResultsContext.jsx
+++ src/components/contexts/OfflineResultsContext.jsx
@@ -105,7 +105,7 @@ export const OfflineResultsProvider = ({ children }) => {
                 setLoading(false)
             }
         })
-    }, [gateway, modelName, projectName, refreshTimer])
+    }, [modelName, projectName, refreshTimer])
 
     // --------------------------------------
     // Renders the provider and its children:
